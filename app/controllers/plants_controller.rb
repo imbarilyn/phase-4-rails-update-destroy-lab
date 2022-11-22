5f32ef64e46c6742011445189ddf54b36b6cacca
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-22 19:35:42 +0300

    completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :response_not_found
 
   # GET /plants
   def index
@@ -8,7 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,7 +19,33 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+      render json: plant
+    # rescue ActiveRecord::RecordNotFound
+    #   response_not_found
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+      head :no_content
+  # rescue ActiveRecord::RecordNotFound
+  #     response_not_found
+end
+
   private
+   
+  def response_not_found
+    render json: {error: "plant not found"}, status: 404
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
